61a4033195746bfe7f918fdbe3a306fcc8fcf80e
aleksandr popov <sanchos8_7@mail.ru>
2023-05-16 17:24:19 +0300

    16-05-2023

--- js/menu-active.js
+++ js/menu-active.js
@@ -4,6 +4,20 @@ document.querySelectorAll(".main-menu li a").forEach((el) => {
   }
 });
 
+/* === IBG === */
+function ibg() {
+  let ibg = document.querySelectorAll("._ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+    }
+    if (ibg[i].querySelector('source') && ibg[i].querySelector('source').getAttribute('srcset') != null) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('source').getAttribute('srcset') + ')';
+    }
+  }
+}
+ibg();
+
 
 
 
